c476a34287772ba8e76135faff1a491613ef8b16
Antonio Chan <antonio.chan@emory.edu>
2017-09-09 20:21:03 -0400

    second commit

--- PennApps_Bomo/JumpARViewController.swift
+++ PennApps_Bomo/JumpARViewController.swift
@@ -1,69 +1,69 @@
+////
+////  ViewController.swift
+////  PennApps_Bomo
+////
+////  Created by Antonio on 9/8/17.
+////  Copyright © 2017 Antonio. All rights reserved.
+////
 //
-//  ViewController.swift
-//  PennApps_Bomo
+//import UIKit
+//import SceneKit
+//import ARKit
 //
-//  Created by Antonio on 9/8/17.
-//  Copyright © 2017 Antonio. All rights reserved.
+//class JumpARViewController: UIViewController, ARSCNViewDelegate {
 //
-
-import UIKit
-import SceneKit
-import ARKit
-
-class JumpARViewController: UIViewController, ARSCNViewDelegate {
-    
-    @IBOutlet var sceneView: ARSCNView!
-    
-    let didPop = UserDefaults.standard
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Set the view's delegate
-        sceneView.delegate = self
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
-        
-        
-        let jumpPop = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "jumpPop") as! JumpPop
-        
-        self.addChildViewController(jumpPop)
-        jumpPop.view.frame = self.view.frame
-        self.view.addSubview(jumpPop.view)
-        jumpPop.didMove(toParentViewController: self)
-        
-        
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-        
-        // Run the view's session
-        sceneView.session.run(configuration)
-        
-        
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        
-        // Pause the view's session
-        sceneView.session.pause()
-    }
-    
-    override var prefersStatusBarHidden : Bool {
-        return true
-    }
-    
-    
-}
-
-
+//    @IBOutlet var sceneView: ARSCNView!
+//
+//    let didPop = UserDefaults.standard
+//
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        // Set the view's delegate
+//        sceneView.delegate = self
+//
+//        // Create a new scene
+//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+//
+//        // Set the scene to the view
+//        sceneView.scene = scene
+//
+//
+//        let jumpPop = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "jumpPop") as! JumpPop
+//
+//        self.addChildViewController(jumpPop)
+//        jumpPop.view.frame = self.view.frame
+//        self.view.addSubview(jumpPop.view)
+//        jumpPop.didMove(toParentViewController: self)
+//
+//
+//    }
+//
+//    override func viewWillAppear(_ animated: Bool) {
+//        super.viewWillAppear(animated)
+//
+//        // Create a session configuration
+//        let configuration = ARWorldTrackingConfiguration()
+//
+//        // Run the view's session
+//        sceneView.session.run(configuration)
+//
+//        
+//    }
+//
+//    override func viewWillDisappear(_ animated: Bool) {
+//        super.viewWillDisappear(animated)
+//
+//        // Pause the view's session
+//        sceneView.session.pause()
+//    }
+//
+//    override var prefersStatusBarHidden : Bool {
+//        return true
+//    }
+//
+//
+//}
+//
+//
